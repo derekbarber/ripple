0e73bf22e2a1d89a1b03d281c12a747fd4c5ebad
Derek Barber <db@derekbarber.ca>
2012-03-01 22:23:11 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ripple::Application.config.secret_token = '1cbb678592a798d3ec49cc9ecd9b26e20a5e8024bf6197fa839013f72a9c32c060f8c3b326ac6c5d3b0c9afc42307665b9b90e430c2a0c2cfbbf794968e15585'
