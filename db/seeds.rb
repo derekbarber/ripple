b559411882a73d978249c49d8c614b1255248c26
Derek Barber <db@derekbarber.ca>
2012-07-25 22:06:10 -0700

    Added a seed data entry for a user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'derek', password: 'foobar', password_confirmation: 'foobar', admin: true, status: true)
+
